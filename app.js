ceb0f9cebdb503c4a5436f272e6ff055c0cededa
maxime <mxderouet@gmail.com>
2021-03-03 14:02:36 +0100

    get/post route for api

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Maxime:test@cluster0.2qyyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://mxderouet:<password>@cluster0.2qyyr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const Thing = require('./models/thing');
 
-MongoClient.connect(uri,
+mongoose.connect(uri,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch((error) => console.log(error.message, 'Connexion à MongoDB échouée !'));
 
 
 const app = express();
@@ -41,11 +41,12 @@ app.post('/api/stuff', (req, res, next) => {
     ...req.body
   });
   thing.save()
-    .then(() => res.status(201).json({ message: ' !'}))
+    .catch((error) => { console.log(error.message)})
+    .then(() => res.status(201).json({ message: 'Object saved!'}))
     .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
@@ -59,13 +60,13 @@ app.get('/api/stuff/:id', (req, res, next) => {
 
 app.put('/api/stuff/:id', (req, res, next) => {
   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .then(() => res.status(200).json({ message: 'Object updated !'}))
     .catch(error => res.status(400).json({ error }));
 });
 
 app.delete('/api/stuff/:id', (req, res, next) => {
   Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .then(() => res.status(200).json({ message: 'Object deleted !'}))
     .catch(error => res.status(400).json({ error }));
 });
 
